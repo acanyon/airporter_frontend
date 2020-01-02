6cc9a3d328a607b062ff2e910f980478d80d7042
Alexis Canyon <acanyon@gmail.com>
2020-01-01 23:21:07 -0800

    fixed heroku

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: ["./src/index.js"],
+  entry: ["./src/index.jsx"],
   output: {
     path: path.resolve(__dirname),
     filename: 'bundle.js'
