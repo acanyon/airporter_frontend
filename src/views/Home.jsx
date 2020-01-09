417252a10a2988b6a78d009f2d1bf9f6c098cea0
Alexis Canyon <acanyon@gmail.com>
2020-01-09 14:06:07 -0800

    refactor form to make more sense

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -11,10 +11,11 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      departureCity: 'San Francisco',
-      departureDate: '8/29/2020',
-      returnCity: 'San Diego',
-      returnDate: '8/31/2020',
+      departureCity: '',
+      departureDate: '',
+      arrivalCity: '',
+      returnDate: '',
+      roundTrip: true,
       passengers: '1',
     };
   }
@@ -25,50 +26,71 @@ class Home extends Component {
         <div className={styles.flightSubmissionContainer}>
           <div className={styles.formTitle}>Search Travel Availability</div>
           <br/>
-          <div>Departure</div>
+          <br/>
           <div className={styles.inputRow}>
-            <input
-              className={styles.input}
-              value={this.state.departureCity}
-              onChange={event => this.setState({departureCity: event.currentTarget.value})}
-            />
-            <div className={styles.inputDateContainer}>
+            <div className={styles.inputColumn}>
+              <div>Departure</div>
+              <input
+                className={styles.input}
+                value={this.state.departureCity}
+                onChange={event => this.setState({departureCity: event.currentTarget.value})}
+                placeholder='San Francisco'
+              />
+            </div>
+            <div className={styles.inputColumn}>
+              <div>Arrival</div>
+              <input
+                className={styles.input}
+                value={this.state.arrivalCity}
+                onChange={event => this.setState({arrivalCity: event.currentTarget.value})}
+                placeholder='San Diego'
+              />
+            </div>
+          </div>
+          <br/>
+          <div className={styles.inputRow}>
+            <div className={styles.inputColumn}>
+              <div>Departure Date</div>
               <input
                 className={styles.input}
                 value={this.state.departureDate}
                 onChange={event => this.setState({departureDate: event.currentTarget.value})}
+                placeholder='1/21'
               />
             </div>
-          </div>
-          <br/>
-          <br/>
-          <div>Return</div>
-          <div className={styles.inputRow}>
-            <input
-              className={styles.input}
-              value={this.state.returnCity}
-              onChange={event => this.setState({returnCity: event.currentTarget.value})}
-            />
-            <div className={styles.inputDateContainer}>
+            <div className={styles.inputColumn}>
+              <div>Return Date</div>
               <input
                 className={styles.input}
                 value={this.state.returnDate}
                 onChange={event => this.setState({returnDate: event.currentTarget.value})}
+                disabled={!this.state.roundTrip}
+                placeholder='2/3'
               />
+              <div>
+                Round-Trip:
+                <input
+                  type="checkbox"
+                  checked={this.state.roundTrip}
+                  onChange={() => this.setState({roundTrip: !this.state.roundTrip})}
+                />
+              </div>
             </div>
           </div>
           <br/>
-          <br/>
           <div>Passenger Count</div>
-          <input
-            className={styles.input}
-            value={this.state.passengers}
-            onChange={event => this.setState({passengers: event.currentTarget.value})}
-          />
+          <div className={styles.inputRow}>
+            <i className={`fas fa-user ${styles.userIcon}`}></i>
+            <input
+              className={styles.input}
+              value={this.state.passengers}
+              onChange={event => this.setState({passengers: event.currentTarget.value})}
+            />
+          </div>
           <br/>
           <br/>
           <br/>
-          <Link to="/travel_results" className={styles.formSubmitButton}>GO!</Link>
+          <Link to="/travel_results" className={styles.formSubmitButton}>Search</Link>
           <br/>
           <br/>
         </div>
@@ -81,7 +103,7 @@ class Home extends Component {
       <div>
         <div className={styles.body}>
           <div className={styles.container}>
-            <div className={styles.heroText}>The reolvutionary portal airport</div>
+            <div className={styles.heroText}>The REVOLUTIONARY portal airport</div>
             <div className={styles.column}>
               {this.renderForm()}
             </div>
